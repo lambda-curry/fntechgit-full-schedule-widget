c51947c0d636f248a67d6d506083b3b5fa785b44
Mai Vang <mai.vang@deque.com>
2022-03-02 19:42:45 -0600

    feat: a11y issues fixed: tabbing, screenreader, grouping

--- src/components/button-bar/index.js
+++ src/components/button-bar/index.js
@@ -1,21 +1,34 @@
-import React from 'react';
+import { set } from 'lodash';
+import React, { useState } from 'react';
 import {Button, ToggleButton, ToggleButtonGroup} from "react-bootstrap";
 
 import styles from './index.module.scss';
 
 const ButtonBar = ({view, timezone, summitTimezoneLabel, onChangeView, onChangeTimezone, onSync, onShare}) => {
     const timezoneLabel = timezone === 'local' ? 'Your Local Timezone' : summitTimezoneLabel;
+    
+    const timezoneButtonToggle = () => {
+        onChangeTimezone(timezone === 'local' ? 'show' : 'local');
+        setTimezoneToggle(prevCheck => !prevCheck);
+    }
+
+    const [timezoneToggle, setTimezoneToggle] = useState(false);
+    const [viewToggle, setViewToggle] = useState();
 
     return (
         <div className={styles.wrapper}>
             <ToggleButton
-                checked={timezone === 'local'}
+                role="button"
+                tabIndex={0}
                 value={0}
                 type="checkbox"
                 className={`${styles.button} ${styles.timezoneBtn}`}
-                onChange={() => onChangeTimezone(timezone === 'local' ? 'show' : 'local')}
+                aria-labelledby="timezone"
+                aria-pressed={timezoneToggle}
+                onChange={() => timezoneButtonToggle()}
             >
-                <i className="fa fa-clock-o" aria-hidden="true" /> {timezoneLabel}
+                <i className="fa fa-clock-o" aria-hidden="true" /> 
+                <span id="timezone">{timezoneLabel}</span>
             </ToggleButton>
             <div className={styles.buttonGroup}>
                 <div className={styles.firstGroup}>
@@ -30,12 +43,26 @@ const ButtonBar = ({view, timezone, summitTimezoneLabel, onChangeView, onChangeT
                 </div>
                 <div className={styles.divider} />
                 <div className={styles.secondGroup}>
-                    <ToggleButtonGroup name="view" value={view} onChange={onChangeView}>
-                        <ToggleButton value="calendar" className={styles.button}>
+                    <ToggleButtonGroup role="group" aria-label="Schedule view" name="view" value={view} onChange={onChangeView}>
+                        <ToggleButton 
+                            value="calendar" 
+                            className={styles.button}
+                            role="button"
+                            tabIndex={0} 
+                            aria-pressed={viewToggle}
+                            onChange={() => setViewToggle(prevCheck => !prevCheck)}
+                            >
                             <i className="fa fa-calendar-o" aria-hidden="true" />
                             Calendar
                         </ToggleButton>
-                        <ToggleButton value="list" className={styles.button}>
+                        <ToggleButton 
+                            value="list" 
+                            className={styles.button}
+                            role="button"
+                            tabIndex={0} 
+                            aria-pressed={viewToggle}
+                            onChange={() => setViewToggle(prevCheck => !prevCheck)}
+                            >
                             <i className="fa fa-list" aria-hidden="true" />
                             List
                         </ToggleButton>
