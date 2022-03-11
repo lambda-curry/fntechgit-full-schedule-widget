2d00365e3a0932ad95794aefcc43f823f8a2d45d
Tomás Castillo <tomrndom@users.noreply.github.com>
2022-03-11 13:29:16 -0300

    Change in styles and position calculation (#14)
    * Change in styles and position calculation
    
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>
    
    * Adding comments to explain the logic of top calculations
    
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>

--- src/components/calendar/index.js
+++ src/components/calendar/index.js
@@ -78,11 +78,12 @@ const Calendar = ({events, settings, summit, addEventToSchedule, removeEventFrom
     const filteredGroupedEvents = groupedEvents.filter(d => d.hours.length);
 
     const onEventClick = (ev, event) => {
+        // Getting the position relative to the viewport, if it's higher than certain values, it substract an amount 
+        // to keep the event info popup inside the viewport.
         const rect = ev.target.getBoundingClientRect();
-        const top = rect.top < 150 ? 10 : rect.top - 150;
-        const scroll = window?.scrollY || 0;
+        const top = rect.top < 150 ? 10 : rect.top > 750 ? rect.top - 700 : rect.top > 400 ? rect.top - 350 : rect.top - 150;
 
-        setInfoPos([top + scroll, ev.clientX + 30]);
+        setInfoPos([top, ev.clientX + 30]);
         setEventDetails(event);
     };
 
