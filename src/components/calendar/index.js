40fd6f1cd7421eda3d2aa33be186b2742c61197a
Tomás Castillo <tomrndom@users.noreply.github.com>
2022-04-04 12:08:12 -0300

    Hotfix/event info popup position (#16)
    * Change position to absolute, adjust calculation of popup position
    
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>
    
    * Adjusting top value for first events
    
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>

--- src/components/calendar/index.js
+++ src/components/calendar/index.js
@@ -78,12 +78,14 @@ const Calendar = ({events, settings, summit, addEventToSchedule, removeEventFrom
     const filteredGroupedEvents = groupedEvents.filter(d => d.hours.length);
 
     const onEventClick = (ev, event) => {
+        const rect = ev.target.getBoundingClientRect();
+        const scroll = window?.scrollY || 0;
+        
         // Getting the position relative to the viewport, if it's higher than certain values, it substract an amount 
         // to keep the event info popup inside the viewport.
-        const rect = ev.target.getBoundingClientRect();
-        const top = rect.top < 150 ? 10 : rect.top > 750 ? rect.top - 700 : rect.top > 400 ? rect.top - 350 : rect.top - 150;
+        const top = rect.top < 150 ? - 250 : rect.top > 750 ? rect.top - 850 : rect.top > 400 ? rect.top - 550 : rect.top - 300;
 
-        setInfoPos([top, ev.clientX + 30]);
+        setInfoPos([top + scroll, ev.clientX + 30]);
         setEventDetails(event);
     };
 
