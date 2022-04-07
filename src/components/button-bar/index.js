992a9992678a1af29629bee15997cfcee5c1fe47
Carlo Piantini <carlo.piantini@gmail.com>
2022-04-07 11:19:11 -0400

    feat: Upgrading react-bootstrap to v2.2.1

--- src/components/button-bar/index.js
+++ src/components/button-bar/index.js
@@ -1,66 +1,96 @@
-import { set } from 'lodash';
 import React from 'react';
-import { Button, ToggleButton, ToggleButtonGroup } from "react-bootstrap";
+import { Button, ToggleButton, ToggleButtonGroup } from 'react-bootstrap';
+import classNames from 'classnames';
 
 import styles from './index.module.scss';
 
-const ButtonBar = ({ view, timezone, summitTimezoneLabel, onChangeView, onChangeTimezone, onSync, onShare }) => {
-    const isLocalTZ = timezone === 'local';
-    const timezoneLabel = isLocalTZ ? 'Your Local Timezone' : summitTimezoneLabel;
-    
-    return (
-        <div className={styles.wrapper}>
-            <Button
-                role="button"
-                tabIndex={0}
-                className={`${styles.button} ${styles.timezoneBtn} ${isLocalTZ ? 'active':''}`}
-                aria-labelledby="timezone"
-                aria-pressed={isLocalTZ}
-                onClick={() => onChangeTimezone(isLocalTZ ? 'show' : 'local')}
-            >
-                <i className="fa fa-clock-o" aria-hidden="true" /> 
-                <span id="timezone">{timezoneLabel}</span>
-            </Button>
-            <div className={styles.buttonGroup}>
-                <div className={styles.firstGroup}>
-                    <Button onClick={onSync} className={`${styles.button} ${styles.cal}`}>
-                        <i className="fa fa-refresh" aria-hidden="true" />
-                        Calendar Sync
-                    </Button>
-                    <Button onClick={onShare} className={`${styles.button} ${styles.share}`}>
-                        <i className="fa fa-share" aria-hidden="true" />
-                        Share
-                    </Button>
-                </div>
-                <div className={styles.divider} />
-                <div className={styles.secondGroup}>
-                    <ToggleButtonGroup role="group" aria-label="Schedule view" name="view" value={view} onChange={onChangeView}>
-                        <ToggleButton 
-                            value="calendar" 
-                            className={styles.button}
-                            role="button"
-                            tabIndex={0} 
-                            aria-pressed={view === "calendar"}
-                            >
-                            <i className="fa fa-calendar-o" aria-hidden="true" />
-                            Calendar
-                        </ToggleButton>
-                        <ToggleButton 
-                            value="list" 
-                            className={styles.button}
-                            role="button"
-                            tabIndex={0} 
-                            aria-pressed={view === "list"}
-                            >
-                            <i className="fa fa-list" aria-hidden="true" />
-                            List
-                        </ToggleButton>
-                    </ToggleButtonGroup>
-                </div>
-            </div>
-        </div>
-    );
-};
+const ButtonBar = ({
+  view,
+  timezone,
+  summitTimezoneLabel,
+  onChangeView,
+  onChangeTimezone,
+  onSync,
+  onShare,
+}) => {
+  const isLocalTZ = timezone === 'local';
+  const timezoneLabel = isLocalTZ ? 'Your Local Timezone' : summitTimezoneLabel;
 
+  return (
+    <div className={styles.wrapper}>
+      <Button
+        role='button'
+        tabIndex={0}
+        className={`${styles.button} ${styles.timezoneBtn} ${
+          isLocalTZ ? 'active' : ''
+        }`}
+        aria-labelledby='timezone'
+        aria-pressed={isLocalTZ}
+        onClick={() => onChangeTimezone(isLocalTZ ? 'show' : 'local')}
+      >
+        <i className='fa fa-clock-o' aria-hidden='true' />
+        <span id='timezone'>{timezoneLabel}</span>
+      </Button>
+
+      <div className={styles.buttonGroup}>
+        <div className={styles.firstGroup}>
+          <Button onClick={onSync} className={`${styles.button} ${styles.cal}`}>
+            <i className='fa fa-refresh' aria-hidden='true' />
+            Calendar Sync
+          </Button>
+
+          <Button
+            onClick={onShare}
+            className={`${styles.button} ${styles.share}`}
+          >
+            <i className='fa fa-share' aria-hidden='true' />
+            Share
+          </Button>
+        </div>
+
+        <div className={styles.divider} />
+
+        <div className={styles.secondGroup}>
+          <ToggleButtonGroup
+            aria-label='Schedule view'
+            name='view'
+            onChange={onChangeView}
+            role='group'
+            type='radio'
+            value={view}
+          >
+            <ToggleButton
+              aria-pressed={view === 'calendar'}
+              className={classNames(styles.button, {
+                active: view === 'calendar',
+              })}
+              id='view-calendar'
+              role='button'
+              tabIndex={0}
+              value='calendar'
+            >
+              <i className='fa fa-calendar-o' aria-hidden='true' />
+              Calendar
+            </ToggleButton>
+
+            <ToggleButton
+              aria-pressed={view === 'list'}
+              className={classNames(styles.button, {
+                active: view === 'list',
+              })}
+              id='view-list'
+              role='button'
+              tabIndex={0}
+              value='list'
+            >
+              <i className='fa fa-list' aria-hidden='true' />
+              List
+            </ToggleButton>
+          </ToggleButtonGroup>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default ButtonBar;
