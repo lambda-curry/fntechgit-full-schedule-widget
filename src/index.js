358d2a60addb673281522b379b559c4b44508044
CarloPiantini <carlo.piantini@gmail.com>
2022-04-12 18:53:24 -0400

    feat: Upgrading react-bootstrap to v2.2.1 (#19)
    * feat: Upgrading react-bootstrap to v2.2.1
    
    * chore: Resolving PR comments.

--- src/index.js
+++ src/index.js
@@ -17,39 +17,42 @@ import FullSchedule from './full-schedule';
 
 import EventsData from './dummy_data/events.json';
 import SummitData from './dummy_data/summit.json';
-import marketingSettings from './dummy_data/marketing-data.json'
+import marketingSettings from './dummy_data/marketing-data.json';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const scheduleProps = {
-    events: EventsData,
-    summit: SummitData.summit,
-    marketingSettings: marketingSettings.colors,
-    userProfile: null,
-    colorSource: 'track',
-    view: 'calendar',
-    timezone: 'show',
-    now: null,
-    title: 'Custom Schedule',
-    withThumbs: false,
-    defaultImage: 'https://image.shutterstock.com/image-photo/bright-spring-view-cameo-island-260nw-1048185397.jpg',
-    onEventClick: console.log,
-    onStartChat: console.log,
-    showSendEmail: true,
-    shareLink: 'santi.com/share',
-    needsLogin: () => console.log('login needed'),
-    triggerAction: (action, payload) => new Promise((resolve, reject) => {
-        setTimeout(() => {
-            console.log('loading...', action, payload);
-            resolve(payload);
-        }, 500)
-    })
+  events: EventsData,
+  summit: SummitData.summit,
+  marketingSettings: marketingSettings.colors,
+  userProfile: null,
+  colorSource: 'track',
+  view: 'calendar',
+  timezone: 'show',
+  now: null,
+  title: 'Custom Schedule',
+  withThumbs: false,
+  defaultImage:
+    'https://image.shutterstock.com/image-photo/bright-spring-view-cameo-island-260nw-1048185397.jpg',
+  onEventClick: console.log,
+  onStartChat: console.log,
+  showSendEmail: true,
+  shareLink: 'santi.com/share',
+  needsLogin: () => console.log('login needed'),
+  triggerAction: (action, payload) =>
+    new Promise((resolve, reject) => {
+      setTimeout(() => {
+        console.log('loading...', action, payload);
+        resolve(payload);
+      }, 500);
+    }),
 };
-
 
 // width 780px or 230px
 
 ReactDOM.render(
-    <div style={{maxWidth: '800px', margin: '0 auto'}}>
-        <FullSchedule {...scheduleProps} />
-    </div>,
-    document.querySelector('#root')
+  <div style={{ maxWidth: '800px', margin: '0 auto' }}>
+    <FullSchedule {...scheduleProps} />
+  </div>,
+  document.querySelector('#root')
 );
