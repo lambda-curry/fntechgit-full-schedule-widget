ab74ca87768fb86bb54b57fa1f03ec0c38dafd78
Carlo Piantini <carlo.piantini@gmail.com>
2022-04-13 17:27:49 -0400

    feat: Horizontal flow of events in calendar view.

--- src/components/calendar/day/index.js
+++ src/components/calendar/day/index.js
@@ -4,25 +4,30 @@ import Hour from '../hour';
 
 import styles from './index.module.scss';
 
-
-const Day = ({dateString, dateStringDay, hours, nowUtc, onEventClick}) => {
-
-    return (
-        <div className={styles.wrapper}>
-            <div className={styles.dayLabel}>
-                <span className={styles.day}>{dateStringDay}</span>, {dateString}
-            </div>
-            <div>
-                {hours.map(hour => <Hour {...hour} nowUtc={nowUtc} onEventClick={onEventClick} key={`cal-hr-${hour.hour}`} />)}
-            </div>
-        </div>
-    );
+const Day = ({ dateString, dateStringDay, hours, nowUtc, onEventClick }) => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.dayLabel}>
+        <span className={styles.day}>{dateStringDay}</span>, {dateString}
+      </div>
+      <div>
+        {hours.map((hour) => (
+          <Hour
+            {...hour}
+            nowUtc={nowUtc}
+            onEventClick={onEventClick}
+            key={`cal-hr-${hour.hour}`}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 Day.propTypes = {
-    dateString: PropTypes.string.isRequired,
-    dateStringDay: PropTypes.string.isRequired,
-    hours: PropTypes.array.isRequired,
+  dateString: PropTypes.string.isRequired,
+  dateStringDay: PropTypes.string.isRequired,
+  hours: PropTypes.array.isRequired,
 };
 
 export default Day;
--- src/components/calendar/event/index.js
+++ src/components/calendar/event/index.js
@@ -1,48 +1,53 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import LiveLine from "../live-line";
 
 import styles from './index.module.scss';
 
-const Event = ({event, position, nowUtc, onEventClick}) => {
-    const eventStyles = {
-      backgroundColor: event.eventColor
-    };
+const Event = ({ event, onEventClick }) => {
+  const eventStyles = {
+    backgroundColor: event.eventColor,
+  };
 
-    const getHosts = () => {
-        let hosts = [];
-        if (event.speakers?.length > 0) {
-            hosts = [...event.speakers];
-        }
-        if (event.moderator) hosts.push(event.moderator);
+  const getHosts = () => {
+    let hosts = [];
+    if (event.speakers?.length > 0) {
+      hosts = [...event.speakers];
+    }
+    if (event.moderator) hosts.push(event.moderator);
 
-        return hosts;
-    };
+    return hosts;
+  };
 
-    const speakers = getHosts().map(
-        (s, i) =>
-            <React.Fragment key={`ev-${event.id}-spkr-${s.id}`}>
-                {i > 0 ? ', ' : ''}<span className={styles.speaker}>{s.first_name} {s.last_name}</span>
-            </React.Fragment>
-    );
+  const speakers = getHosts().map((s, i) => (
+    <React.Fragment key={`ev-${event.id}-spkr-${s.id}`}>
+      {i > 0 ? ', ' : ''}
+      <span className={styles.speaker}>
+        {s.first_name} {s.last_name}
+      </span>
+    </React.Fragment>
+  ));
 
-    return (
-        <div className={styles.outerWrapper}>
-            <LiveLine start={event.start_date} end={event.end_date} offset={position} now={nowUtc} />
-            <div id={`event-${event.id}`} className={`${styles.wrapper} event-wrapper`} style={eventStyles} onClick={ev => onEventClick(ev, event)}>
-                <div className={styles.title}>{event.title}</div>
-                {speakers.length > 0 &&
-                <div className={styles.speakers}>By {speakers}</div>
-                }
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles.outerWrapper}>
+      <div
+        id={`event-${event.id}`}
+        className={`${styles.wrapper} event-wrapper`}
+        style={eventStyles}
+        onClick={(ev) => onEventClick(ev, event)}
+      >
+        <p className={styles.title}>{event.title}</p>
+        {speakers.length > 0 && (
+          <p className={styles.speakers}>By {speakers}</p>
+        )}
+      </div>
+    </div>
+  );
 };
 
 Event.propTypes = {
-    event: PropTypes.object.isRequired,
-    position: PropTypes.number.isRequired,
-    nowUtc: PropTypes.number.isRequired
+  event: PropTypes.object.isRequired,
+  position: PropTypes.number.isRequired,
+  nowUtc: PropTypes.number.isRequired,
 };
 
 export default Event;
