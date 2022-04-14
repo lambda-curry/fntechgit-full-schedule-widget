230fbbfe3f1ee2f225071b9ccde8feb06990abd7
CarloPiantini <carlo.piantini@gmail.com>
2022-04-14 10:53:57 -0400

    feat: Horizontal flow of events in calendar view. (#20)
    * feat: Horizontal flow of events in calendar view.
    
    * chore: Removing unused style.

--- src/components/calendar/hour/index.js
+++ src/components/calendar/hour/index.js
@@ -1,34 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Event from '../event';
-import LiveLine from '../live-line';
 
 import styles from './index.module.scss';
 
-const Hour = ({hourLabel, events, nowUtc, onEventClick}) => {
-    return (
-        <div className={styles.wrapper}>
-            <div className={styles.timeWrapper}>
-                <div className={styles.time}>{hourLabel}</div>
-            </div>
-            <div className={styles.eventsWrapper}>
-                {events.map((ev, idx) =>
-                    <Event
-                        event={ev}
-                        position={idx}
-                        nowUtc={nowUtc}
-                        onEventClick={onEventClick}
-                        key={`cal-ev-${ev.id}`}
-                    />
-                )}
-            </div>
-        </div>
-    );
+const Hour = ({ hourLabel, events, nowUtc, onEventClick }) => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.timeWrapper}>
+        <div className={styles.time}>{hourLabel}</div>
+      </div>
+      <div className={styles.eventsWrapper}>
+        {events.map((ev, idx) => (
+          <Event
+            event={ev}
+            position={idx}
+            nowUtc={nowUtc}
+            onEventClick={onEventClick}
+            key={`cal-ev-${ev.id}`}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 Hour.propTypes = {
-    hourLabel: PropTypes.string.isRequired,
-    events: PropTypes.array.isRequired,
+  hourLabel: PropTypes.string.isRequired,
+  events: PropTypes.array.isRequired,
 };
 
 export default Hour;
